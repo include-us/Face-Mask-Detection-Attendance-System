3a0e191384ea08026b183a7a945890759cf40eae
darshjain <darshj786@gmail.com>
2021-01-30 14:32:07 +0530

    Readme initialised and format update

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Attendance-System
-It is a basic attendance system based on GUI and uses sql database
+It is a basic attendance system based on GUI and uses SQLITE database  
+<br>
+
+### CONTRIBUTERS   
+1. Darsh Jain
+2. Ankita Upadhyay
+3. Manav Arya
+4. Asjad Nirban   
+<br>
+
+### FILE STRUCTURE
+
+1. `` admin-database.py``
+Holds all functions related to sqlite database
+
+2. `` mask-detection.py ``
+Holds all functions to detect mask
+
+3. ``main.py`` 
+Is the driver code
--- admin_database.py
+++ admin_database.py
@@ -2,45 +2,54 @@ import sqlite3 as db
 import os
 from sqlite3 import Error
 import datetime
+import tkinter as tk
+
 class database:
     def sql_connection():
         try:
-            con=db.connect("databases/users_info.db")
+            con = db.connect("databases/users_info.db")
             return con
         except Error:
             print(Error)
 
-    #-----------------TESTING ONLY PHASE----------------
+    # -----------------TESTING ONLY PHASE----------------
     def sql_table(con):
         cursor_obj = con.cursor()
         try:
-            cursor_obj.execute("CREATE TABLE users(id integer PRIMARY KEY, name text, year real, department text, position text, hireDate text)")
+            cursor_obj.execute(
+                "CREATE TABLE users(id integer PRIMARY KEY, name text, year real, department text, position text, hireDate text)")
             con.commit()
         except:
             print("TABLE EXISTS")
+
     def add_user(con, entities):
         try:
             cursor_obj = con.cursor()
-            cursor_obj.execute('INSERT INTO users(id, name, year, department, position, hireDate) VALUES(?, ?, ?, ?, ?, ?)', entities)
+            cursor_obj.execute(
+                'INSERT INTO users(id, name, year, department, position, hireDate) VALUES(?, ?, ?, ?, ?, ?)', entities)
             con.commit()
         except:
             print("PRIMARY KEY EXISTS")
-    def search_user(con,search_id):
+
+    def search_user(con, search_id):
         try:
-            cursor_obj=con.cursor()
-            cursor_obj.execute("SELECT name,year FROM users WHERE id= ?",str(search_id))
-            rows=cursor_obj.fetchall()
+            cursor_obj = con.cursor()
+            cursor_obj.execute(
+                "SELECT name,year FROM users WHERE id= ?", str(search_id))
+            rows = cursor_obj.fetchall()
             for i in rows:
                 print(rows)
         except:
             print("Does not exist")
-    def delete_user(con,delete_id):
+
+    def delete_user(con, delete_id):
         try:
-            cursor_obj=con.cursor()
-            cursor_obj.execute("DELETE FROM users WHERE id= ?",str(delete_id))
+            cursor_obj = con.cursor()
+            cursor_obj.execute("DELETE FROM users WHERE id= ?", str(delete_id))
             print("DELETED")
             con.commit()
         except:
             print("CANT DELETE USER")
+
     def __init__(self) -> None:
-        con = database.sql_connection()      
+        con = database.sql_connection()
