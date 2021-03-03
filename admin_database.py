3378c960c72f48c62ac51d875f91db9391861d53
darshjain <darshj786@gmail.com>
2021-03-03 11:13:13 +0530

    cleaning and working tree and minor fixes

--- admin_database.py
+++ admin_database.py
@@ -3,7 +3,23 @@ import os
 from sqlite3 import Error
 import datetime
 import tkinter as tk
-
+from tkinter import messagebox
 
 class database:
-    print("ACTIVE")
+    def __init__(self):
+        self.con=db.connect('databases//users_info.db')
+        self.curr=self.con.cursor()
+    def add_user(self,id,name,year,dept,barcode):
+        try:
+            command="INSERT INTO users VALUES('"+str(id)+"','"+name+"','"+str(year)+"','"+dept+"','"+barcode+"')"
+            self.curr.execute(command)
+            self.con.commit()
+            messagebox.showinfo("SUCCESS","USER ADDED")
+        except:
+            messagebox.showerror("Error","Couldnt add the user")
+    def delete_user(self,id):
+        try:
+            self.curr.execute("DELETE FROM users WHERE id=?",id)
+            self.con.commit()
+        except:
+            messagebox.showerror("Error","Failed to delete user")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from admin_database import database 
-from windows.admin_login import administrator 
 from windows.open_panel import opening_panel
 
 task=opening_panel()
