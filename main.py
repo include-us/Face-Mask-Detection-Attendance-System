bf6be6f0dd3fddd081459bd724b7710fc3db6327
darshjain <darshj786@gmail.com>
2021-03-08 18:53:08 +0530

    Viewer In LISTS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from windows.open_panel import opening_panel
 
-task=opening_panel()
-task.open_panel()
+if __name__ == "__main__":
+    task = opening_panel()
+    task.open_panel()
--- windows/database_windows.py
+++ windows/database_windows.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 from tkinter import messagebox
+from admin_database import database as db
+from tkinter import *
 
 
 class database_methods(object):
+    def view_command(self):
+        self.list.delete(0, END)
+        self.srows = db()
+        rows = self.srows.view_users()
+        for row in rows:
+            self.list.insert(END, row)
+
+    # --------------------------Windows----------------------------
     def add_window(self):
         self.window_add = tk.Tk()
         self.window_add.geometry("400x200")
@@ -19,8 +29,23 @@ class database_methods(object):
 
     def view_window(self):
         self.window_view = tk.Tk()
-        self.window_view.geometry("400x200")
+        self.window_view.geometry("500x400")
         self.window_view.title("View All Users")
+        self.list = tk.Listbox(self.window_view, height=25, width=65)
+        self.scroller = tk.Scrollbar(self.window_view)
+        self.btn_update = tk.Button(
+            self.window_view,
+            text="Update",
+            height=1,
+            width=20,
+            command=self.view_command,
+        )
+        self.btn_update.grid(row=30, column=0, columnspan=2)
+        self.scroller.grid(row=2, column=2, rowspan=6)
+        self.list.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+        self.list.configure(yscrollcommand=self.scroller.set)
+        self.scroller.configure(command=self.list.yview)
 
         self.window_view.mainloop()
 
