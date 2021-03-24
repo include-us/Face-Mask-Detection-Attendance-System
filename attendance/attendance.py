562c3a7f2e786f83bc4469f97ffa37f40168d129
darshjain <darshj786@gmail.com>
2021-03-24 15:35:41 +0530

    date time column made

--- attendance/attendance.py
+++ attendance/attendance.py
@@ -1,12 +1,29 @@
 import csv
-from ..admin_database import database as db
-from ..barcode.barcode import barcode_read as read_code
+import datetime
+# from ..barcode.barcode import barcode_read as read_code
+# from ..admin_database import database as db
+import pandas as pd
+
 
 class attendance(object):
     def __init__(self):
-        self.con = db.connect('databases//users_info.db')
-        self.curr = self.con.cursor()
+        # self.con = db.connect('databases//users_info.db')
+        # self.curr = self.con.cursor()
+        self.file = open("attendance/data.csv")
+
     def mark(self):
-        print("MARKED")
+        # self.code=read_code.reader()
+        print("MARKING")
+        df = pd.read_csv("attendance/data.csv")
+        if datetime.date in df.columns:
+            pass
+        else:
+            x = datetime.datetime.now()
+            df[x.strftime("%x")] = ""
+            df.to_csv("attendance/data.csv", index=False)
+
     def show(self):
         print("SHOWN")
+
+
+attendance().mark()
