cd6ea3b7ed0c37314134998ba576a4f6b3ab2dfc
darshjain <darshj786@gmail.com>
2021-03-31 16:49:08 +0530

    Formatting

--- windows/database_windows.py
+++ windows/database_windows.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
-from admin_database import database as db 
+from admin_database import database as db
 from tkinter import *
 import subprocess
 from subprocess import Popen
 import os
+
+
 class database_methods(object):
     def view_command(self):
         self.list.delete(0, END)
@@ -20,7 +22,7 @@ class database_methods(object):
             handle.delete_user(id)
         except:
             messagebox.showerror("Error", "COULDNT DELETE USER")
-            
+
     # --------------------------Windows----------------------------
 
     def add_window(self):
@@ -29,34 +31,42 @@ class database_methods(object):
         self.window_add.title("Add A User")
         self.window_add.configure(background="turquoise")
 
-        lab01 = tk.Label(self.window_add, text="", bg="turquoise", fg="dark slate gray", font=("times", 30, 'bold'))
-        lab02 = tk.Label(self.window_add, text="Add User", bg="turquoise", fg="dark slate gray", font=("times", 30, 'bold'))
-        lab03 = tk.Label(self.window_add, text="", bg="turquoise", fg="dark slate gray", font=("times", 30, 'bold'))
+        lab01 = tk.Label(self.window_add, text="", bg="turquoise",
+                         fg="dark slate gray", font=("times", 30, 'bold'))
+        lab02 = tk.Label(self.window_add, text="Add User", bg="turquoise",
+                         fg="dark slate gray", font=("times", 30, 'bold'))
+        lab03 = tk.Label(self.window_add, text="", bg="turquoise",
+                         fg="dark slate gray", font=("times", 30, 'bold'))
 
-        self.lbl_id1 = tk.Label(self.window_add,text="Name: ", font=("times", 10, 'bold'),bg="turquoise", fg="Black",height=2,width=20,)
+        self.lbl_id1 = tk.Label(self.window_add, text="Name: ", font=(
+            "times", 10, 'bold'), bg="turquoise", fg="Black", height=2, width=20,)
         self.entry_id1 = tk.Entry(self.window_add, width=45,)
 
-        self.lbl_id2 = tk.Label(self.window_add,text="Year: ", font=("times", 10, 'bold'),bg="turquoise", fg="Black",height=2,width=20,)
+        self.lbl_id2 = tk.Label(self.window_add, text="Year: ", font=(
+            "times", 10, 'bold'), bg="turquoise", fg="Black", height=2, width=20,)
         self.entry_id2 = tk.Entry(self.window_add, width=45,)
 
-        self.lbl_id3 = tk.Label(self.window_add,text="Department: ", font=("times", 10, 'bold'),bg="turquoise", fg="Black",height=2,width=20,)
+        self.lbl_id3 = tk.Label(self.window_add, text="Department: ", font=(
+            "times", 10, 'bold'), bg="turquoise", fg="Black", height=2, width=20,)
         self.entry_id3 = tk.Entry(self.window_add, width=45,)
 
-        lab04 = tk.Label(self.window_add, text="", bg="turquoise", fg="dark slate gray", font=("times", 30, 'bold'))
-        self.btn_add = tk.Button(self.window_add,text="Add Student",font=("times", 10, 'bold'),bg="old lace",height=3,width=42,command=self.window_add)
-        
-        lab01.grid(row=0,column=0)
-        lab02.grid(row=1,column=1,columnspan=7)
-        lab03.grid(row=3,column=0) 
+        lab04 = tk.Label(self.window_add, text="", bg="turquoise",
+                         fg="dark slate gray", font=("times", 30, 'bold'))
+        self.btn_add = tk.Button(self.window_add, text="Add Student", font=(
+            "times", 10, 'bold'), bg="old lace", height=3, width=42, command=self.window_add)
 
-        self.lbl_id1.grid(row=5,column=1)
-        self.entry_id1.grid(row=5,column=2)
-        self.lbl_id2.grid(row=7,column=1)
-        self.entry_id2.grid(row=7,column=2)
-        self.lbl_id3.grid(row=9,column=1)
-        self.entry_id3.grid(row=9,column=2)
-        lab04.grid(row=10,column=3)
-        self.btn_add.grid(row=11,column=1,columnspan=5) 
+        lab01.grid(row=0, column=0)
+        lab02.grid(row=1, column=1, columnspan=7)
+        lab03.grid(row=3, column=0)
+
+        self.lbl_id1.grid(row=5, column=1)
+        self.entry_id1.grid(row=5, column=2)
+        self.lbl_id2.grid(row=7, column=1)
+        self.entry_id2.grid(row=7, column=2)
+        self.lbl_id3.grid(row=9, column=1)
+        self.entry_id3.grid(row=9, column=2)
+        lab04.grid(row=10, column=3)
+        self.btn_add.grid(row=11, column=1, columnspan=5)
 
         self.window_add.mainloop()
 
@@ -65,10 +75,14 @@ class database_methods(object):
         self.window_delete.geometry("400x250")
         self.window_delete.configure(background="turquoise")
         self.window_delete.title("Delete A User")
-        lab01 = tk.Label(self.window_delete, text="", bg="turquoise", fg="dark slate gray", font=("times", 15, 'bold'))
-        lab02 = tk.Label(self.window_delete, text="Delete a user", bg="turquoise", fg="dark slate gray", font=("simplified arabic fixed", 15, 'bold'))
-        lab03 = tk.Label(self.window_delete, text="", bg="turquoise", fg="dark slate gray", font=("times", 15, 'bold'))
-        lab04 = tk.Label(self.window_delete, text="", bg="turquoise", fg="dark slate gray", font=("times", 15, 'bold'))
+        lab01 = tk.Label(self.window_delete, text="", bg="turquoise",
+                         fg="dark slate gray", font=("times", 15, 'bold'))
+        lab02 = tk.Label(self.window_delete, text="Delete a user", bg="turquoise",
+                         fg="dark slate gray", font=("simplified arabic fixed", 15, 'bold'))
+        lab03 = tk.Label(self.window_delete, text="", bg="turquoise",
+                         fg="dark slate gray", font=("times", 15, 'bold'))
+        lab04 = tk.Label(self.window_delete, text="", bg="turquoise",
+                         fg="dark slate gray", font=("times", 15, 'bold'))
         self.lbl_id = tk.Label(
             self.window_delete,
             text="Enter ID to be Deleted: ",
